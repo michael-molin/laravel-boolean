2eaefaecec66a221982730e602625fc57243165a
Michael <m.molin.1987@gmail.com>
2020-06-03 15:44:18 +0200

    Models & migrations

---
+++ app/Page.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+
+    // gestisco i campi da riempire
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'summary',
+        'body',
+        'visible'
+    ];
+
+    // inizializzo relazioni tabelle
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
+
+    public function photos()
+    {
+        return $this->belongsToMany('App\Photo');
+    }
+}
